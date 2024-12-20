1b343cdeff78bce1ec2d9faf638af02eec906683
kuba <kuba456@gmail.com>
2024-12-19 20:05:16 -0800

    Setup remotes

---
+++ oiio_setup.sh
@@ -0,0 +1,6 @@
+git clone git@github.com:kaarrot/oiio.git oiio-kaarrot
+cd oiio-kaarrot
+
+git remote add trunk https://github.com/AcademySoftwareFoundation/OpenImageIO.git
+git remote add lgritz  https://github.com/lgritz/OpenImageIO.git
+git remote set-url origin  git@github.com:kaarrot/oiio.git
